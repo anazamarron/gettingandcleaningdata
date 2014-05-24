d17640ba1d96e0551b1d809e534780f499ce8fa9
anazamarron <anazamarron@gmail.com>
2014-05-24 19:40:16 +0200

    Final result of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,20 +22,21 @@ features <- read.table('UCI\ HAR\ Dataset/features.txt',stringsAsFactors=FALSE)
 # gets a vector with the position of the columns with mean and standard deviation
 mean_std <- grep("-mean()|-std()", features[, 2])
 
-# extract the columns we nedd (we get a data.frame as result of this operation)
+# extract the columns we need (we get a data.frame as result of this operation)
 totalX <- totalX[,mean_std]
 
 # Add correct names to the columns and clean the names a little bit
 colnames(totalX) <- features[mean_std, 2]
 colnames(totalX) <- tolower(names(totalX))
 colnames(totalX) <- gsub("\\(\\)", "", names(totalX))
-names(totalX) <- gsub("-","_",names(totalX))
+colnames(totalX) <- gsub("-","_",names(totalX))
 
 
 # 3. Uses descriptive activity names to name the activities in the data set
 
-activities <- read.table('UCI\ HAR\ Dataset/activity_labels.txt',stringsAsFactor=FALSE)
-activities[, 2] <- tolower(as.character(activities[, 2]))
+Activities <- read.table('UCI\ HAR\ Dataset/activity_labels.txt',stringsAsFactor=FALSE)
+Activities[, 2] <- tolower(as.character(Activities[, 2]))
+colnames(Activities)<-c("id","activity")
 
 # assign the value of avtivity to the totalY data.frame
 totalY[,1] <- activities[totalY[,1], 2]
@@ -45,33 +46,50 @@ colnames(totalY) <- "activities"
 colnames(totalSubject) <- "subject"
 totalData <- cbind(totalSubject,totalY, totalX)
 
+# Order the data.frame by subject and activities
+totalData <- totalData[with(totalData,order(subject,activities)),]
+
+# Write data.frame into a file called cleaned_data.txt
 write.table(totalData, 'UCI\ HAR\ Dataset/clean_data.txt')
 
 
 # 5. Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
 
-uniqueSubjects = unique(S)[,1]
-numSubjects = length(unique(S)[,1])
-numActivities = length(activities[,1])
-numCols = dim(cleaned)[2]
-result = cleaned[1:(numSubjects*numActivities), ]
+# obtain the subjects, order them and count them:
+Subjects <- unique(totalSubject)[,1]
+a <- Subjects[order(Subjects)]
+numSubjects <- length(a)
 
+# We allready have the activities in the activities darta.frame, order them and count them:
+numActivities <- length(Activities[,1])
+
+# Count the number of colums of cleaned data
+numColumns <- dim(totalData)[2]
+
+# Initializes a data.frame with the total number of columns 
+# and the total number of rows (Subjects * Activity)
+# And assing the tudy names
+
+finalData<-data.frame(matrix(ncol = numColumns, nrow = numSubjects*numActivities))
+colnames(finalData) <- colnames(totalData)
+
+# Start looping the subjects and activities, getting the mean of each column for them
 row = 1
 for (s in 1:numSubjects) {
         for (a in 1:numActivities) {
-                result[row, 1] = uniqueSubjects[s]
-                result[row, 2] = activities[a, 2]
-                tmp <- cleaned[cleaned$subject==s & cleaned$activity==activities[a, 2], ]
-                result[row, 3:numCols] <- colMeans(tmp[, 3:numCols])
-                row = row+1
+                #subject
+                finalData[row,1] <- Subjects[s]
+                #activity name
+                finalData[row,2] <- Activities[a,2]
+                # subset of this subject and this activity
+                temporal <- subset(totalData, subject==s)
+                temporal <- subset(temporal, activities == Activities[a,1])
+                #mean of the values
+                finalData[row,3:numColumns] <- colMeans(temporal[, 3:numColumns],na.rm=TRUE)
+                row <-row+1
+                
         }
 }
-write.table(result, "data_set_with_the_averages.txt")
 
-# res2 <- read.table("data_set_with_the_averages.txt")
-# result[4,4]
-# res2[4,4]
-# res2[4,4]==result[4,4]
-# result[6,4]
-# res2[6,4]
-# res2[6,4]==result[6,4]
+# And write the result to disc
+write.table(finalData, "averages_subject_activity.txt")
